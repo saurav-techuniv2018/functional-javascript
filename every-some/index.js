f5b7eecb7d81915f157e6f12a228c69bb12d6960
Saurav Sahu <sauravsahu.12@live.com>
2018-01-16 14:50:57 +0530

    Test passed when passed array is not a subset of all good users.

--- every-some/index.js
+++ every-some/index.js
@@ -12,4 +12,11 @@ let input1 = [
     { id: 2 },
     { id: 1 }
 ];
-console.log('Test when submitted users are a subset of goodUsers:', testAllValid(input1) === true);
+console.log('Test when submitted users array is a subset of goodUsers:', testAllValid(input1) === true);
+
+let input2 = [
+    { id: 2 },
+    { id: 1 },
+    { id: 7 }
+];
+console.log('Test when submitted users array is not a subset of goodUsers:', testAllValid(input2) === false);
