76eda819bba0221f604bf17f52db08b82fa038dd
Saurav Sahu <sauravsahu.12@live.com>
2018-01-17 10:36:06 +0530

    Add input with the same value multiple times.

--- reduce/index.js
+++ reduce/index.js
@@ -2,9 +2,7 @@ let compareObjects = function (firstObject, secondObject) {
     return JSON.stringify(firstObject) === JSON.stringify(secondObject);
 }
 
-
 let countWords = require('./countWords');
-
 
 let submittedUsers = [];
 console.log('Test for empty input: ',
@@ -13,3 +11,7 @@ console.log('Test for empty input: ',
 submittedUsers = ['a', 'a', 'b', 'c', 'a', 'd', 'c', 'b'];
 console.log('Test for input with strings:',
     compareObjects(countWords(submittedUsers), { 'a': 3, 'b': 2, 'c': 2, 'd': 1 }));
+
+submittedUsers = ['a', 'a', 'a', 'a'];
+console.log('Test for inputs with the same value multiple times:',
+    compareObjects(countWords(submittedUsers), { 'a': 4 }));
