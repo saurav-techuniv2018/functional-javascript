6a25f07c1fd81eb602255df615f815c1df660c27
Saurav Sahu <sauravsahu.12@live.com>
2018-01-24 05:54:03 +0530

    async-loops - update test
    async-loops - update test

--- async-loops/load-users.js
+++ async-loops/load-users.js
@@ -1,13 +1,14 @@
 async function loadUsers(userIds, load, done) {
   let result = [];
+  let loadedCount = 0;
 
   userIds.forEach((id) => {
     load(id, function (user, userId) {
       result[userId] = user;
+      loadedCount++;
+      if (loadedCount === userIds.length) done(result);
     });
   });
-
-  done(result);
 };
 
 module.exports = loadUsers;
